422d9a31726a4952691da7bf677949eebc806a24
Mr. Wolfe <116601200+Wolfe-SFUSD@users.noreply.github.com>
2024-12-18 09:46:13 -0800

    Update test_concatFirstAndLastNames.py

--- test_concatFirstAndLastNames.py
+++ test_concatFirstAndLastNames.py
@@ -7,6 +7,6 @@ first_names2 = ["Bruce", "Clark", "Diana", "Barry", "Hal", "Oliver", "Arthur", "
 last_names2 = ["Wayne", "Kent", "Prince", "Allen", "Jordan", "Queen", "Curry", "Stone", "Batson", "Constantine"]
 
 def test_concatFirstAndLastNames():
-    assert concatFirstAndLastNames(first_names1,last_names2) == ['Peter Parker', 'Tony Stark', 'Steve Rogers', 'Natasha Romanoff', 'Bruce Banner', 'Thor Odinson', 'Wanda Maximoff', 'Scott Lang', 'Stephen Strange']
+    assert concatFirstAndLastNames(first_names1,last_names1) == ['Peter Parker', 'Tony Stark', 'Steve Rogers', 'Natasha Romanoff', 'Bruce Banner', 'Thor Odinson', 'Wanda Maximoff', 'Scott Lang', 'Stephen Strange']
     assert concatFirstAndLastNames(first_names2,last_names2) == ['Bruce Wayne', 'Clark Kent', 'Diana Prince', 'Barry Allen', 'Hal Jordan', 'Oliver Queen', 'Arthur Curry', 'Victor Stone', 'Billy Batson', 'John Constantine']
 
